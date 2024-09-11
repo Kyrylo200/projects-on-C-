5fe9b90902f33bf03a1f6ad2b4b0beaed5355813
Kyrylo200 <kirillchernyshev000@gmail.com>
2024-09-11 14:01:42 +0200

    migration1

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,37 +1,8 @@
 ﻿using System.Text;
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-//we use the upper code to find a library 
-Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
-Console.WriteLine("Invoice: 1021\t\tComplete!");
-Console.WriteLine("Invoice: 1022\t\tComplete!");
-Console.WriteLine("\nOutput Directory:\t");
-Console.WriteLine(@"  c:\source\repos
-                   (this is where your file goes)");
-Console.Write(@" c: invoices");
-// Kon'nichiwa World
 Console.OutputEncoding = Encoding.UTF8;
-
-Console.WriteLine("\n\u3053\u3093\u306B\u3061\u306F World!");
-// To generate Japanese invoices:
-// Nihon no seikyū-sho o seisei suru ni wa:
-Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
-// User command to run an application
-Console.WriteLine(@"c:\invoices\app.exe -j");
-Console.Write("Hello" + " PAPA");
-//we use the plus symbol to concatenate 
-string firstName = "Bob";
-string greeting = "Hello";
-//string message = greeting + " " + firstName + "!";
-
-//we can wrtie the same string with string interpolation 
-string message = $"{greeting} {firstName}!";
-Console.WriteLine("\n" + message);
-//text interpolation as well can be used to write text with the variabloes
-int version = 11;
-string updateText = "Update to Windows";
-string mesage = $"{updateText} {version}";
-Console.WriteLine(mesage);
-string projectsName = "First-Project";
-Console.WriteLine($@"C:\Output\{projectsName}\Data");
+string projectName = "ACME";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+Console.WriteLine($@"View English Output:
+                         C:\Exercise\{projectName}.txt\n\n");
+Console.WriteLine($@"{russianMessage}
+                         C:\Exercise\{projectName}\ru-RU\data.txt");                         
