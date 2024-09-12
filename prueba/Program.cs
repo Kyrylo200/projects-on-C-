308a5f5bca8a043b938a85132b5f55d58a1a69f4
Kyrylo200 <kirillchernyshev000@gmail.com>
2024-09-12 12:56:20 +0200

    temperature

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,8 +1,5 @@
-﻿using System.Text;
-Console.OutputEncoding = Encoding.UTF8;
-string projectName = "ACME";
-string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
-Console.WriteLine($@"View English Output:
-                         C:\Exercise\{projectName}.txt\n\n");
-Console.WriteLine($@"{russianMessage}
-                         C:\Exercise\{projectName}\ru-RU\data.txt");                         
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Xml.XPath;
+int farenheit = 94;
+decimal result = (decimal)(farenheit - 32)* 5 / 9;
+Console.WriteLine(Math.Round(result,1));
--- prueba/obj/Debug/net8.0/prueba.AssemblyInfo.cs
+++ prueba/obj/Debug/net8.0/prueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c62e75bdb6411d19f52956867cae8e786a845d46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fe9b90902f33bf03a1f6ad2b4b0beaed5355813")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
